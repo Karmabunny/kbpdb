e6e3d14bc54b2699aff3ddad6440f29c23057e68
Gwilyn <gwilyn@karmabunny.com.au>
2021-06-04 17:53:44 +0930

    Stub query test.

---
+++ tests/PdbQueryTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use karmabunny\pdb\PdbLog;
+use karmabunny\pdb\PdbParser;
+use karmabunny\pdb\PdbSync;
+use kbtests\Database;
+use PHPUnit\Framework\TestCase;
+
+
+class PdbQueryTest extends TestCase
+{
+
+    public function testBasic(): void
+    {
+        $pdb = Database::getConnection();
+
+
+    }
+
+
+    public function testJoin(): void
+    {
+
+    }
+}
