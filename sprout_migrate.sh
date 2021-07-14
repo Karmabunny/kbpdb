9ee10a586348c98d27ee7deff8436cd6bc09a88e
Gwilyn <gwilyn@karmabunny.com.au>
2021-07-14 17:02:15 +0930

    Touch up sprout migrate script.
    - Ignore .git
    - Auto inject compat libs
    - Include config snippet

--- sprout_migrate.sh
+++ sprout_migrate.sh
@@ -4,7 +4,7 @@ set -e
 FIND='use (Sprout\\Exceptions\\)(QueryException|TransactionRecursionException|ConstraintQueryException|RowMissingException);'
 REPLACE='use karmabunny\\pdb\\Exceptions\\\2;'
 
-grep -rlE "$FIND" "$1" | while read line; do
+grep -rlE "$FIND" "$1" | grep -v .git | while read line; do
     echo "$line"
     sed -i.bak -E "s/$FIND/$REPLACE/" $line
     rm $line.bak
@@ -12,21 +12,26 @@ done
 
 echo "Done"
 echo
-echo
-cat - <<PDB
+
+PDB="$(find $1 -iname Pdb.php -exec realpath {} \; | grep -v .git | grep -v vendor)"
+echo "Writing: $PDB"
+cat > $PDB << 'PDB'
 <?php
 namespace Sprout\Helpers;
 
-class Pdb extends karmabunny\pdb\Compat\Pdb {}
+class Pdb extends \karmabunny\pdb\Compat\Pdb {}
 PDB
 
+echo "ok"
 echo
-echo
-cat - <<DBSYNC
+
+DBSYNC="$(find $1 -iname DatabaseSync.php -exec realpath {} \; | grep -v .git | grep -v vendor)"
+echo "Writing: $DBSYNC"
+cat > $DBSYNC << 'DBSYNC'
 <?php
 namespace Sprout\Helpers;
 
-class DatabaseSync extends karmabunny\pdb\Compat\DatabaseSync
+class DatabaseSync extends \karmabunny\pdb\Compat\DatabaseSync
 {
     /**
      * Load the db_struct.xml from core and from all modules
@@ -43,6 +48,21 @@ class DatabaseSync extends karmabunny\pdb\Compat\DatabaseSync
             }
         }
     }
-
 }
 DBSYNC
+
+echo "ok"
+echo
+echo
+echo "=== Pop this in Kohana::setup() ==="
+echo
+
+cat - << 'KOHANA'
+$config = Kohana::config('database.default.connection');
+$config['prefix'] = 'sprout_';
+$config['character_set'] = Kohana::config('database.default.character_set');
+Pdb::config($config);
+KOHANA
+
+echo
+echo
