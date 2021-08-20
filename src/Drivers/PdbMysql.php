474af139dac9df9861662ee01e96d9d6410771f6
Gwilyn <gwilyn@karmabunny.com.au>
2021-08-20 10:34:36 +0930

    Fix tz error messages.

--- src/Drivers/PdbMysql.php
+++ src/Drivers/PdbMysql.php
@@ -40,8 +40,8 @@ class PdbMysql extends Pdb
         }
 
         if (is_string($tz = $hacks[PdbConfig::HACK_TIME_ZONE] ?? false)) {
-            if (!is_string($tz) or !preg_match('!^[^/0-9]+/[^/0-9]+$!', $tz)) {
-                $tz = is_object($tz) ? gettype($tz) : (string) $tz;
+            if (!is_string($tz) and !preg_match('!^[^/0-9]+/[^/0-9]+$!', $tz)) {
+                $tz = is_scalar($tz) ? (string) $tz : gettype($tz);
                 throw new InvalidArgumentException("Invalid time zone: {$tz}");
             }
 
