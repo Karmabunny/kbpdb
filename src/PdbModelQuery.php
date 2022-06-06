c72ff5b48b10e1f096aa575ec26ff30f760dfb82
Gwilyn <gwilyn@karmabunny.com.au>
2022-06-06 17:59:42 +0930

    Use the pdb inflector for creating table aliases in model queries.

--- src/PdbModelQuery.php
+++ src/PdbModelQuery.php
@@ -16,6 +16,11 @@ use InvalidArgumentException;
 class PdbModelQuery extends PdbQuery
 {
 
+
+    /** @var bool */
+    public $_inflect = false;
+
+
     /**
      *
      * @param string $model
@@ -27,9 +32,91 @@ class PdbModelQuery extends PdbQuery
             throw new InvalidArgumentException("{$model} must implement PdbModelInterface");
         }
 
-        parent::__construct($model::getConnection());
-        $this->from($model::getTableName());
+        /** @var Pdb $pdb */
+        $pdb = $model::getConnection();
+        $table = $model::getTableName();
+
+        parent::__construct($pdb);
+        $this->from($table);
         $this->as($model);
     }
 
+
+    /**
+     * Enable table name inflection.
+     *
+     * This will automatically alias table names for FROM and JOIN statements.
+     *
+     * Example:
+     * ```sprout_sites => site```
+     *
+     * @param bool $inflect
+     * @return static
+     */
+    public function inflect(bool $inflect = true)
+    {
+        $this->_inflect = $inflect;
+        return $this;
+    }
+
+
+    /** @inheritdoc */
+    public function build(): array
+    {
+        if (!$this->_inflect) {
+            return parent::build();
+        }
+
+        $query = clone $this;
+        $query->inflect(false);
+
+        $query->_from = [];
+        $query->_joins = [];
+
+        if (count($this->_from) == 1) {
+            $from = reset($this->_from);
+            $from = $this->_inflect($from);
+            $query->from($from);
+        }
+
+        foreach ($this->_joins as [$type, $table, $conditions, $combine]) {
+            if (count($table) == 1) {
+                $table = reset($table);
+                $table = $this->_inflect($from);
+            }
+
+            $query->_join($type, $table, $conditions, $combine);
+
+        }
+
+        return $query->build();
+    }
+
+
+    /**
+     * Create an automatic alias from a field name.
+     *
+     * This strips the prefix and flips the pluralisation.
+     *
+     * Example:
+     * ```sprout_sites => site```
+     *
+     * @param string $field
+     * @return string[] [ field, alias ]
+     */
+    protected function _inflect(string $field): array
+    {
+        $inflector = $this->pdb->getInflector();
+        $prefix = $this->pdb->getPrefix($field);
+
+        if (strpos($field, $prefix) === 0) {
+            $alias = substr($field, strlen($prefix));
+            $alias = $inflector->singular($alias);
+        }
+        else {
+            $alias = $inflector->singular($field);
+        }
+
+        return [$field, $alias];
+    }
 }
