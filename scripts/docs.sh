545c4db6a73e85f98ad5a1506083655aa50e27d5
Gwilyn <gwilyn@karmabunny.com.au>
2022-12-07 13:33:04 +1030

    Add mkdocs + php documentor.

---
+++ scripts/docs.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+cd "$(dirname $0)/.."
+set -e
+
+PHPDOC="./.phpdoc.phar"
+
+# Install phpdoc
+if ! [[ -f $PHPDOC ]]; then
+    wget https://phpdoc.org/phpDocumentor.phar -O $PHPDOC || exit $?
+    chmod +x $PHPDOC
+fi
+
+# Build API classes
+$PHPDOC run \
+    --directory src/ \
+    --target guides/api/ \
+    --no-interaction \
+    --no-ansi
+
+echo
+
+# Install mkdocs
+if ! [[ -f venv/bin/activate ]]; then
+    python3 -m venv venv/
+fi
+
+source venv/bin/activate
+pip install --quiet \
+    mkdocs-material=="8.*"
+
+# Build guides
+mkdocs build
