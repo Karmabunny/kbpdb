37405b8f26047f11d3ab32699a8aa34d8341ed0d
Gwilyn <gwilyn@karmabunny.com.au>
2023-02-14 20:06:21 +1030

    Add tests for aliases.

--- tests/PdbQueryTest.php
+++ tests/PdbQueryTest.php
@@ -55,9 +55,31 @@ class PdbQueryTest extends TestCase
 
         [$sql, $params] = $query->build();
 
-        print_r($sql);
-        echo "\n";
-        print_r($params);
+        $expected = 'SELECT ~project_roles.* FROM ~project_roles ';
+        $expected .= 'INNER JOIN ~projects AS "project" ON project.id = ~project_roles.project_id AND "project"."active" != ? AND "project"."visible_members" != ? AND "project"."profile_id" != ? AND "project"."region_id" IN (?, ?, ?) ';
+        $expected .= 'INNER JOIN ~project_roles_trade_type_ids AS "trade_join" ON trade_join = ~project_rules.id AND "trade_join"."trade_type_id" IN (?, ?, ?) ';
+        $expected .= 'WHERE "active" != ? AND "start_date" BETWEEN ? AND ? AND "end_date" BETWEEN ? AND ? AND ("level1" = ? OR ("level2" = ? AND "level3" = ?)) ';
+        $expected .= 'GROUP BY "project_roles"."id"';
+
+        $this->assertEquals($expected, $sql);
+        $this->assertCount(17, $params);
     }
 
+
+    public function testAlias()
+    {
+        $query = $this->pdb->find('big_thingy');
+
+        [$sql, $params] = $query->build();
+        $expected = 'SELECT ~big_thingy.* FROM ~big_thingy';
+
+        $this->assertEquals($expected, $sql);
+        $this->assertCount(0, $params);
+
+        [$sql, $params] = $query->alias('thing')->build();
+        $expected = 'SELECT "thing".* FROM ~big_thingy AS "thing"';
+
+        $this->assertEquals($expected, $sql);
+        $this->assertCount(0, $params);
+    }
 }
