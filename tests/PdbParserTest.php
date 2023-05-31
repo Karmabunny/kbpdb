67e29d99284075dd123a910a65779400e771f5f1
Gwilyn <gwilyn@karmabunny.com.au>
2023-05-31 15:05:34 +0930

    Add tests for nullable parsing.

--- tests/PdbParserTest.php
+++ tests/PdbParserTest.php
@@ -7,12 +7,41 @@ use PHPUnit\Framework\TestCase;
 class PdbParserTest extends TestCase
 {
 
-    public function testLoad()
+    public function parse()
     {
         $parser = new PdbParser();
         $parser->loadXml(__DIR__ . '/db_struct.xml');
         $parser->sanityCheck();
+        return $parser;
+    }
 
-        echo print_r($parser->getErrors(), true), PHP_EOL;
+
+    public function testLoad()
+    {
+        $parser = $this->parse();
+        $errors = $parser->getErrors();
+        $this->assertEmpty($errors, print_r($errors, true));
+    }
+
+
+    public function testAllowNull()
+    {
+        $parser = $this->parse();
+        $clubs = $parser->getTable('clubs');
+
+        // Explicit true.
+        $column = $clubs->columns['date_deleted'];
+        $this->assertEquals(true, $column->is_nullable);
+        $this->assertEquals(null, $column->default);
+
+        // Explicit false.
+        $column = $clubs->columns['date_added'];
+        $this->assertEquals(false, $column->is_nullable);
+        $this->assertEquals(null, $column->default);
+
+        // Implicit true.
+        $column = $clubs->columns['flags'];
+        $this->assertEquals(true, $column->is_nullable);
+        $this->assertEquals(null, $column->default);
     }
 }
