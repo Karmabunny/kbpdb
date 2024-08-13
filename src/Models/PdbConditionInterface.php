f7ff4893bdc3ce27573493e312f60927c6d59c13
Gwilyn <gwilyn@karmabunny.com.au>
2024-08-14 09:56:35 +1000

    Add condition interface.

---
+++ src/Models/PdbConditionInterface.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @link      https://github.com/Karmabunny
+ * @copyright Copyright (c) 2021 Karmabunny
+ */
+
+namespace karmabunny\pdb\Models;
+
+use karmabunny\pdb\Exceptions\InvalidConditionException;
+use karmabunny\pdb\Pdb;
+use PDOException;
+
+/**
+ *
+ * @package karmabunny\pdb
+ */
+interface PdbConditionInterface
+{
+
+    /**
+     * Build an appropriate SQL clause for this condition.
+     *
+     * The values will be created as ? and added to the $values parameter to
+     * permit one to bind the values later in an safe manner.
+     *
+     * @param Pdb $pdb
+     * @param array $values
+     * @return string
+     * @throws PDOException
+     * @throws InvalidConditionException
+     */
+    public function build(Pdb $pdb, array &$values): string;
+
+
+    /**
+     * Validate this condition.
+     *
+     * @return void
+     * @throws InvalidConditionException
+     * @throws InvalidArgumentException
+     */
+    public function validate();
+
+
+    /**
+     *
+     * @return string
+     */
+    public function getPreviewSql(): string;
+
+}
