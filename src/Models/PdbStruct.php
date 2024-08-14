3138031c7b42009df8e740f235a141e8ae2fd46d
Gwilyn <gwilyn@karmabunny.com.au>
2024-08-14 12:16:50 +1000

    Implement PdbStruct with schema interface.

--- src/Models/PdbStruct.php
+++ src/Models/PdbStruct.php
@@ -2,11 +2,43 @@
 
 namespace karmabunny\pdb\Models;
 
-class PdbStruct
+use karmabunny\pdb\PdbSchemaInterface;
+
+/**
+ * A schema that comes from a `db_struct.xml` file.
+ *
+ * @package karmabunny\pdb
+ */
+class PdbStruct implements PdbSchemaInterface
 {
-    /** @var PdbTable[] */
+
+    /** @var string */
+    public $name;
+
+    /** @var PdbTables[] name => PdbTable */
     public $tables;
 
-    /** @var PdbView[] */
+    /** @var PdbView[] name => PdbView */
     public $views;
+
+
+    /** @inheritdoc */
+    public function getTableNames(): array
+    {
+        return array_keys($this->tables);
+    }
+
+
+    /** @inheritdoc */
+    public function getTables(): array
+    {
+        return $this->tables;
+    }
+
+
+    /** @inheritdoc */
+    public function getViews(): array
+    {
+        return $this->views;
+    }
 }
