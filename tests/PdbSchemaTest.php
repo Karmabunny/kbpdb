097e446516b8516196d1fd4bb3d67609b06cb9ad
Gwilyn <gwilyn@karmabunny.com.au>
2025-02-13 09:23:38 +1100

    Add tests to reapply extracted schemas.

--- tests/PdbSchemaTest.php
+++ tests/PdbSchemaTest.php
@@ -35,7 +35,7 @@ class PdbSchemaTest extends TestCase
     }
 
 
-    public function testSchemaExtraction()
+    public function sync()
     {
         $struct = new PdbParser();
         $struct->loadXml(__DIR__ . '/db_struct.xml');
@@ -46,6 +46,14 @@ class PdbSchemaTest extends TestCase
         $log = $sync->execute();
         // PdbLog::print($log);
 
+        return $struct;
+    }
+
+
+    public function testSchemaExtraction()
+    {
+        $struct = $this->sync();
+
         // Get the schema.
         $schema = $this->pdb->getSchema();
         $tables = $schema->getTables();
@@ -53,6 +61,59 @@ class PdbSchemaTest extends TestCase
         // Basic assertions.
         $this->assertNotEmpty($tables, 'No tables found.');
         $this->assertEquals($struct->getTableNames(), $schema->getTableNames());
+
+        // Detailed assertions.
+        foreach ($tables as $name => $table) {
+            $original = $struct->getTable($name);
+
+            $this->assertNotNull($original);
+
+            // Compare columns.
+            $this->assertEquals(
+                array_keys($original->columns),
+                array_keys($table->columns),
+                "Column mismatch for table: {$name}.",
+            );
+
+            // Compare indexes.
+            $this->assertEquals(
+                array_keys($original->indexes),
+                array_keys($table->indexes),
+                "Index mismatch for table: {$name}.",
+            );
+
+            // Compare foreign keys.
+            $this->assertEquals(
+                array_keys($original->foreign_keys),
+                array_keys($table->foreign_keys),
+                "Foreign key mismatch for table: {$name}.",
+            );
+        }
+    }
+
+
+    public function testSchemaReapply()
+    {
+        $struct = $this->sync();
+
+        // Get the schema.
+        $schema1 = $this->pdb->getSchema();
+        $this->assertNotEmpty($schema1->getTableNames(), 'No tables found.');
+
+        Database::drop('mysql');
+
+        $schema2 = $this->pdb->getSchema();
+        $this->assertEmpty($schema2->getTableNames());
+
+        $sync = new PdbSync($this->pdb);
+        $sync->migrate($struct);
+        $log = $sync->execute();
+        // PdbLog::print($log);
+
+        $schema3 = $this->pdb->getSchema();
+        $this->assertEquals($schema1->getTableNames(), $schema3->getTableNames());
+
+        $tables = $schema3->getTables();
 
         // Detailed assertions.
         foreach ($tables as $name => $table) {
