fe6706ea170bdc6dcb821307a6420828a12178c9
Gwilyn <gwilyn@karmabunny.com.au>
2026-02-24 16:52:08 +1100

    Add tests for strict typed models.
    From this PR:
    https://github.com/Karmabunny/sprout3/pull/175
    
    Co-Authored-By: Benno Lang <benno@truevault.com.au>

--- tests/PdbModelTest.php
+++ tests/PdbModelTest.php
@@ -8,6 +8,7 @@ use kbtests\Database;
 use PHPUnit\Framework\TestCase;
 use kbtests\Models\Club;
 use kbtests\Models\DirtyClub;
+use kbtests\Models\TypedModel;
 use kbtests\Models\SproutItem;
 
 class PdbModelTest extends TestCase
@@ -34,6 +35,7 @@ class PdbModelTest extends TestCase
         if (!Database::isConnected()) $this->markTestSkipped();
 
         $pdb->query('DROP TABLE IF EXISTS ~clubs', [], 'null');
+        $pdb->query('DROP TABLE IF EXISTS ~model_property_tests', [], 'null');
 
         $sync = new PdbSync($pdb);
 
@@ -249,4 +251,113 @@ class PdbModelTest extends TestCase
         $this->assertEquals($model->date_added, $other->date_added);
         $this->assertEquals($model->name, $other->name);
     }
+
+
+    /**
+     * Test that saving and loading data using native types works,
+     * including SET and JSON fields with default values
+     */
+    public function testTypedSaveLoad(): void
+    {
+        $name = 'Test save and load data';
+        $bool_val = false;
+        $int_val = 3;
+        $currency_val = 13.95;
+        $float_val = 1234.56789;
+
+        $dummy = new TypedModel();
+        $dummy->name = $name;
+        $dummy->bool_val = $bool_val;
+        $dummy->int_val = $int_val;
+        $dummy->currency_val = $currency_val;
+        $dummy->float_val = $float_val;
+        $dummy->save();
+
+        $models = TypedModel::findAll();
+        $this->assertEquals(count($models), 1);
+
+        // Confirm that all data saved and loaded correctly from native types
+        /** @var TypedModel $dummy */
+        $dummy = reset($models);
+        $this->assertEquals($dummy->name, $name);
+        $this->assertEquals($dummy->bool_val, $bool_val);
+        $this->assertEquals($dummy->bool_default_false, false); // from default defined in DB
+        $this->assertEquals($dummy->bool_default_true, true); // from default defined in DB
+        $this->assertEquals($dummy->int_val, $int_val);
+        $this->assertEquals($dummy->int_default_zero, 0); // from default defined in DB
+        $this->assertEquals($dummy->int_default_one, 1); // from default defined in DB
+        $this->assertEquals($dummy->currency_val, $currency_val);
+        $this->assertEquals($dummy->float_val, $float_val);
+        $this->assertEquals($dummy->options_db_default, ['a', 'b']); // from default defined in DB
+        $this->assertEquals($dummy->options_model_default, ['c', 'd']); // from default defined in model
+        $this->assertEquals($dummy->json_db_default, [1, 2, ['a' => 'b', 3 => 9]]); // from default defined in DB
+        $this->assertEquals($dummy->json_model_default, ['trash' => 'panda', 123]); // from default defined in model
+        $this->assertEquals($dummy->non_json, '{"do not parse JSON": "for a non-array attribute"}');
+
+        $new_bool = true;
+        $new_int = 5;
+        $new_options = ['a', 'b', 'c'];
+        $new_json = ['x' => 1, 'y' => 543.21, 'z' => ['ciao' => 'hola']];
+        $dummy->bool_val = $new_bool;
+        $dummy->int_val = $new_int;
+        $dummy->options_db_default = $new_options;
+        $dummy->options_model_default = $new_options;
+        $dummy->json_db_default = $new_json;
+        $dummy->json_model_default = $new_json;
+        $dummy->save();
+
+        // Confirm that new data saved and loaded correctly from native types
+        /** @var TypedModel $dummy */
+        $dummy = TypedModel::find(['id' => $dummy->id])->one();
+        $this->assertEquals($dummy->bool_val, $new_bool);
+        $this->assertEquals($dummy->int_val, $new_int);
+        $this->assertEquals($dummy->options_db_default, $new_options);
+        $this->assertEquals($dummy->options_model_default, $new_options);
+        $this->assertEquals($dummy->json_db_default, $new_json);
+        $this->assertEquals($dummy->json_model_default, $new_json);
+    }
+
+
+    /**
+     * Test that overriding defaults works sanely
+     */
+    public function testOverrideDefaults(): void
+    {
+        $dummy = new TypedModel();
+        $dummy->name = 'Test override defaults';
+        $dummy->bool_val = true;
+        $dummy->int_val = 100;
+        $dummy->currency_val = 1.0;
+        $dummy->float_val = 123.45;
+        $dummy->bool_default_false = true;
+        $dummy->bool_default_true = false;
+        $dummy->int_default_zero = 1;
+        $dummy->int_default_one = 0;
+        $dummy->save();
+
+        $models = TypedModel::findAll();
+        $this->assertEquals(count($models), 1);
+
+        /** @var TypedModel $dummy */
+        $dummy = reset($models);
+        $this->assertEquals($dummy->bool_default_false, true);
+        $this->assertEquals($dummy->bool_default_true, false);
+        $this->assertEquals($dummy->int_default_zero, 1);
+        $this->assertEquals($dummy->int_default_one, 0);
+    }
+
+
+    /**
+     * Test that creation using DB defaults doesn't cause a TypeError
+     */
+    public function testFindOrCreate(): void
+    {
+        $dummy = TypedModel::findOrCreate([
+            'name' => 'Test by findOrCreate'
+        ]);
+
+        // Useless assertion just to ensure no TypeError occurs
+        $this->assertInstanceOf(TypedModel::class, $dummy);
+    }
 }
+
