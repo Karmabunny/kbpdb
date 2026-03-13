99de8f554ad552273eed8c25f11ae8da86438daa
Gwilyn <gwilyn@karmabunny.com.au>
2026-03-13 16:28:54 +1100

    Use dotenv for user/pass/db config.

--- tests/config.php
+++ tests/config.php
@@ -8,7 +8,7 @@
 return [
     'type' => 'mysql',
     'host' => getenv('SITES_DB_HOSTNAME') ?: '127.0.0.1',
-    'user' => 'kbpdb',
-    'pass' => 'password',
-    'database' => 'kbpdb',
+    'user' => getenv('SITES_DB_USERNAME') ?: 'kbpdb',
+    'pass' => getenv('SITES_DB_PASSWORD') ?: 'password',
+    'database' => getenv('SITES_DB_DATABASE') ?: 'kbpdb',
 ];
